e2a07612fd09cfa11f3768066d71082e7507fc67
kkk01210 <77380084+kkk01210@users.noreply.github.com>
2021-03-08 20:55:37 +0900

    3.1 까지 들음

--- db.js
+++ db.js
@@ -1,51 +1,16 @@
-export const videos = [
-    {
-        id:11111,
-        title: "Video awesome",
-        description: "This is something I love",
-        views:24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id:121212,
-            name: "김준환",
-            email: "kkk00@1210.com"
-        }
-    },
-    {
-        id:222222,
-        title: "Video super",
-        description: "This is something I love",
-        views:24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id:121212,
-            name: "김준환",
-            email: "kkk00@1210.com"
-        }
-    },
-    {
-        id:333333,
-        title: "Video nice",
-        description: "This is something I love",
-        views:24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id:121212,
-            name: "김준환",
-            email: "kkk00@1210.com"
-        }
-    },
-    {
-        id:44444,
-        title: "Video perfect",
-        description: "This is something I love",
-        views:24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id:121212,
-            name: "김준환",
-            email: "kkk00@1210.com"
-        }
-    }
+import mongoose from "mongoose";
 
-];
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+    useNewUrlParser: true, useFindAndModify:false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = (error) => console.log(`❌ Error on DB Connection:${error}`);
+
+
+
+db.once("open", handleOpen);
+db.on("error", handleError);
