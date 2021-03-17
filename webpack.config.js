3a1965284b37f08b4d9a3653787b3033353c787e
kkk01210 <77380084+kkk01210@users.noreply.github.com>
2021-03-17 18:19:19 +0900

    4.1 고치기 전

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+
+const config = {
+    entry: ENTRY_FILE,
+    mode: MODE,
+    output: {
+        path: OUTPUT_DIR,
+        filename: "[main].[format]"
+    }
+};
+
+
+module.exports = config;
